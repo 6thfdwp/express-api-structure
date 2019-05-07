ae61900380e1efcb5297295facd18d0719bcb1fa
Larry Ding <larryd@wan.gl>
2019-05-07 12:24:19 +1000

    Add develop target in url path

--- src/Config.js
+++ src/Config.js
@@ -2,6 +2,10 @@ class Config {
   static get port() {
     return process.env.PORT || 6000;
   }
+
+  static get deployTarget() {
+    return process.env.DEPLOY_TARGET || 'develop';
+  }
 }
 
 module.exports = Config;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const validateStoreBody = require('../middleware/validateStoreBody');
+const Config = require('../Config');
 const category = require('./category');
 const location = require('./location');
 const search = require('./search');
 const product = require('./product');
 
 const router = express.Router();
-const prefix = `/api/v1`;
+const target = Config.deployTarget;
+const prefix = `/${target}/api/v1`;
 
 router.use(`${prefix}/category`, validateStoreBody, category);
 router.use(`${prefix}/location`, location);
